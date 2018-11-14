7a5d9e24e9d5471d706c8ccee3b289728f70f3fc
ASHISH MANANDHAR <ashish_m1@hotmail.com>
2018-11-14 16:54:36 +1100

    first few lines

--- milestone project 1.py
+++ milestone project 1.py
@@ -1,3 +1,22 @@
 ''' This is 1st milestone project for tic-tac game
 Creator: Ashish Manandhar
 Date: '''
+
+user1 = input("enter username1")
+user2 = input("enter username2")
+user1key = input("{}choose your key X or 0".format(user1))
+user2key = input("{}choose your key X or 0".format(user2))
+print("Starting new game")
+
+def userinput():
+    userinput1 = input("enter your move")
+
+'''check if anyone wins'''
+
+def printtable():
+    print([1,2,3,
+           4,5,6,
+           7,8,9])
+
+userinput()
+printtable()
