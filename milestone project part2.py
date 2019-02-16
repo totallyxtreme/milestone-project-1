814fb3c1a3e930e92e089cda57e2669ccf245cba
ASHISH MANANDHAR <ashish_m1@hotmail.com>
2019-02-16 20:03:46 +1100

    just testing

--- milestone project part2.py
+++ milestone project part2.py
@@ -1,5 +1,38 @@
-print("hello world")
-print('\n'*100)
+import random
 
-print("test")
-print("test again")
+suits = ("Hearts", "Spades", "Diamonds", "Clubs")
+ranks = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+values = {"Ace":11, "Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10}
+
+playing = True
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + ' Of ' + self.suit
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ""
+        for card in self.deck:
+            deck_comp += '\n ' + card.__str__()
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+test_deck = Deck()
+print(test_deck)
